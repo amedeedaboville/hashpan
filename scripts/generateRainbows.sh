b74f274afb5050a208b6591a76a32a991bbe9712
Amedee d'Aboville <amedee.daboville@gmail.com>
2013-12-02 01:00:15 -0500

    Added pansort step to generaterainbows.

--- scripts/generateRainbows.sh
+++ scripts/generateRainbows.sh
@@ -7,8 +7,6 @@ do
   do
     #time ./pangen sha1 $bin $i 500 2000000 $bin
     time ./pangen sha1 $bin $i 250 4000000 $bin
-    #>> "$1.log.txt"
-    filename="sha1_"$i"_500x2000000_$bin.rt"
-    #./rtsort $filename
   done
 done
+./pansort *.rt
