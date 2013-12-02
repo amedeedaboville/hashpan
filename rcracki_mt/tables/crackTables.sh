cdd2bac5a548e30ce3f808e811fd028f055f40c0
Amedee d'Aboville <amedee.daboville@gmail.com>
2013-12-01 23:58:50 -0500

    Added scripts for computation and cracking.

---
+++ rcracki_mt/tables/crackTables.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Here we list the tables in order of popularity of their BINS. This should significantly
+#speed up the process as more hashes are found earlier.
+tablelist=""
+counter=0
+for i in {0..4};
+do
+  for bin in `cat bins.txt`
+  do
+    filename="sha1_"$i"_250x4000000_$bin.rt"
+    if [ -e "$filename" ]
+    then
+      tablelist="$tablelist sha1_"$i"_250x4000000_$bin.rt"
+      let counter=$counter+1
+    fi
+  done
+done
+
+echo "Rainbow Table Cracking with $counter tables:"
+sudo nice -n -20 ../pancrack $tablelist -l hexhashes.txt -t $1 
+#> log.txt
+#; pmset sleepnow
