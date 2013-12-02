754297bb1fd4f2b386815f1df6c014449d66ac4b
Amedee d'Aboville <amedee.daboville@gmail.com>
2013-12-01 20:19:56 -0500

    Added bins that are left.

---
+++ rcracki_mt/tables/generateRainbows.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for bin in `cat $1`
+do
+  echo "Making tables for BIN $bin"
+  for i in {0..4};
+  do
+    #time ./pangen sha1 $bin $i 500 2000000 $bin
+    time ./pangen sha1 $bin $i 400 5000000 $bin
+    #>> "$1.log.txt"
+    filename="sha1_"$i"_500x2000000_$bin.rt"
+    #./rtsort $filename
+  done
+done
